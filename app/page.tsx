17582f977484481c997beb66fa8291148e08c296
Rajesh97266 <rajesh97266@gmail.com>
2025-04-28 11:48:44 +0530

    feat: Implement AppPieChart and AppAreaChart components and integrate into HomePage
    This commit introduces two new chart components, AppPieChart and AppAreaChart, and integrates them into the HomePage to provide additional data visualizations.
    
    Key changes include:
    
    - Creation of `components/AppPieChart.tsx` to implement a pie chart component using recharts, displaying browser usage statistics.
    - Creation of `components/AppAreaChart.tsx` to implement an area chart component using recharts, displaying total visitors data.
    - Modification of `app/page.tsx` to include the `AppPieChart` and `AppAreaChart` components within the responsive grid layout.

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,20 @@
+import AppAreaChart from "@/components/AppAreaChart";
 import AppBarChart from "@/components/AppBarChart";
+import AppPieChart from "@/components/AppPieChart";
 
 const HomePage = () => {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
       <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
-       <AppBarChart />
+        <AppBarChart />
       </div>
       <div className="bg-primary-foreground p-4 rounded-lg">Test2</div>
-      <div className="bg-primary-foreground p-4 rounded-lg">Test2</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">
+        <AppPieChart />
+      </div>
       <div className="bg-primary-foreground p-4 rounded-lg">Test2</div>
       <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
-        Test1
+        <AppAreaChart />
       </div>
       <div className="bg-primary-foreground p-4 rounded-lg">Test2</div>
     </div>
